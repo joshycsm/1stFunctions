1927444263e6f02198dc63dfa53bf89d7e0d20a6
github username <github email address>
2020-03-02 10:54:08 -0700

    creates example functions and shows the difference between function declarion and function expression + prompt, alert, console.log examples

--- app.js
+++ app.js
@@ -36,3 +36,47 @@ var a = multiply(5, 10);
 console.log(a);
 
 console.log(multiply(3, 4));
+
+// New Topic keyless car challenge
+
+// Function Declaration
+function checkDriverAge() {
+  var age = prompt("What is your age?");
+
+  if (Number(age) < 18) {
+    alert("Sorry, you are too young to drive this car. Powering off");
+  } else if (Number(age) > 18) {
+    alert("Powering On. Enjoy the ride!");
+  } else if (Number(age) === 18) {
+    alert("Congratulations on your first year of driving. Enjoy the ride!");
+  }
+}
+
+// console.log(checkDriverAge(20));
+
+// Function Expression
+checkDriverAge2 = function() {
+  var age = prompt("What is your age?");
+
+  if (Number(age) < 18) {
+    alert("Sorry, you are too young to drive this car. Powering off");
+  } else if (Number(age) > 18) {
+    alert("Powering On. Enjoy the ride!");
+  } else if (Number(age) === 18) {
+    alert("Congratulations on your first year of driving. Enjoy the ride!");
+  }
+};
+
+// console.log(checkDriverAge2);
+
+function checkDriverAgeWithArgument(age) {
+  if (Number(age) < 18) {
+    return "Sorry, you are too yound to drive this car. Powering off";
+  } else if (Number(age) > 18) {
+    return "Powering On. Enjoy the ride!";
+  } else if (Number(age) === 18) {
+    return "Congratulations on your first year of driving. Enjoy the ride!";
+  }
+}
+
+checkDriverAgeWithArgument(22);
